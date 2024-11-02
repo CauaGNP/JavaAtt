4cdbf7d03e9c260efdc66acc68a08339337c2ffb
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2024-11-02 03:14:01 +0000

    end att

--- Att5.java
+++ Att5.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class att5 {
+public class Att5 {
     public static void selectionSort(int[] array) {
         int n = array.length;
         for (int i = 0; i < n - 1; i++) {
@@ -30,11 +30,24 @@ public class att5 {
     }
 
     public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
 
+        System.out.println("Resposta 1°:");
+        System.out.println(" ");
+
+        System.out.println(" Selection Sort");
+        System.out.println("Caso Médio e Pior: O(n^2) - pois, em ambos os casos, o algoritmo percorre todo o array para encontrar o menor elemento");
+        System.out.println("e o coloca na posição correta, repetindo esse processo para cada posição");
+        System.out.println(" ");
+
+        System.out.println("Insertion Sort");
+        System.out.println("Caso Médio e Pior: O(n^2) - o algoritmo precisa inserir cada elemento na posição correta, comparando");
+        System.out.println(" com os elementos anteriores. No melhor caso (array já ordenado), a complexidade é O(n).");
+        System.out.println(" ");
+
+        System.out.println("Resposta 2°(Vantagem dos algoritimos):");
+        System.out.println(" Insertion Sort é mais vantajoso para arrays parcialmente ordenados ou pequenos. Ele ajusta rapidamente os elementos para a posição correta com menos trocas e comparações.");
+        System.out.println("  Selection Sort é preferido quando o número de trocas precisa ser minimizado, pois o algoritmo faz apenas n - 1 trocas, independentemente da ordenação inicial dos elementos.");
+        System.out.println(" ");
 
         int[] sizes = {100};
         Random rand = new Random();
